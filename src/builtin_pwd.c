3aaa853e1b9279b8bcd5ab073b715fd4688f3d0d
Younghak Shin <yshin@c2r10s8.42seoul.kr>
2020-09-23 19:04:47 +0900

    add errmsg in builtin_pwd.c

--- src/builtin_pwd.c
+++ src/builtin_pwd.c
@@ -1,11 +1,20 @@
 #include "../includes/minishell.h"
 
+/*
+** builtin_pwd: get current working directory path using getcwd().
+** if getcwd() fails, it returns -1 and pwd prints error msg.
+** if success, pwd prints the path of current working directory.
+*/
 void	builtin_pwd(void)
 {
 	char	buf[MAXPATHLEN];
 	char	*ptr;
 
-	ptr = getcwd(buf, MAXPATHLEN);
+	if (!(ptr = getcwd(buf, MAXPATHLEN)))
+	{
+		write(2, "pwd: Failed to get path. Check the buffer size.\n", 48);
+		return ;
+	}
 	write(1, ptr, ft_strlen(ptr));
 	write(1, "\n", 1);
 }
