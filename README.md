a6569ae93200c4cfc2a7979aec06dfc467e98daa
sungslee <k8440009@gmail.com>
2020-09-24 17:06:40 +0900

    add temp_prompt

--- README.md
+++ README.md
@@ -4,3 +4,15 @@ minishell
 1. subject 번역
 2. 기본적인 fuctions 공부
 3. 프로그램 작성
+4. memory leak 확인
+- ps를 사용하여 process_id를 찾는다.
+
+while;
+do leaks (process_id);
+sleep 5;
+clean;
+done
+5. crtl + c 로 종료하지 못한 경우
+- ps를 사용하여 process_id를 찾는다.
+- kill (process_id)
+- 입력하여 강제 종료한다.
--- src/main.c
+++ src/main.c
@@ -1,6 +1,48 @@
 #include "../includes/minishell.h"
 
-int main()
+#define MAX_PATH 100
+#define LF 10
+
+/*
+**	buffer에는 절대경로가 저장되어 ㅇㅆ다.
+*/
+int			print_prompt()
+{
+	char	buffer[MAX_PATH];
+	char	*value;
+	char	*line;
+	int		ret;
+
+	value = getcwd(buffer, MAX_PATH);
+
+	if (value != 0)
+	{
+		ft_putstr("catshell@");
+		ft_putstr_fd(buffer, 1);
+		ft_putstr(" ");
+
+		while ((ret = get_next_line(0, &line)))
+		{
+			if (ret == -1)
+				printf("get_next_line error\n");
+
+			ft_putstr(line);
+			ft_putchar_fd('\n', 1);
+			free(line);
+	
+			ft_putstr("catshell@");
+			ft_putstr_fd(buffer, 1);
+			ft_putstr(" ");
+		}
+		free(line);
+	}
+	return (1);
+}
+
+int			main()
 {
 	printf("hello world\n");
+
+	print_prompt();
+	return (0);
 }
