991c79c46fe4df001f09c5c671bab413a669dcf8
sungslee <k8440009@gmail.com>
2020-10-20 14:11:04 +0900

    update type.h, add table type

--- includes/types.h
+++ includes/types.h
@@ -13,6 +13,7 @@
 #ifndef TYPES_H
 # define TYPES_H
 
+typedef int		t_bool;
 /*
 ** tokenizer.c
 */
@@ -24,5 +25,59 @@ typedef struct	s_tokenizer
 	int		prev;
 	char	quote;
 }				t_tokenizer;
+/*
+**	lexer.c
+*/
+typedef struct	s_lexer
+{
+	char	*type;
+	char	**seqs;
+	char	**format;
+	int		len;
+	int		idx;
+	int		i;
+	int		j;
+	int		res;
+}				t_lexer;
+/*
+**	parser.c
+*/
+typedef struct	s_parser
+{
+	int		command;
+	int		fd;
+}				t_parser;
+/*
+**	table structure with almost files
+*/
+typedef struct	s_redir
+{
+	char	*sign;
+	char	*arg;
+	int		fd;
+	int		error;
+	struct	s_redir	*next;
+}				t_redir;
 
+typedef struct	s_command
+{
+	char		*cmd;
+	char		**arg_list;
+	int			idx;
+}				t_command;
+
+typedef	struct	s_job
+{
+	struct	s_command	command;
+	struct	s_redir		*redir_list;
+	struct	s_job		*next;
+}				t_job;
+
+typedef struct	s_table
+{
+	struct s_job		*job_list;
+	int					sep_type;
+	int					fd;
+	struct	s_table		*next;
+}				t_table;
 #endif
