57d4f0bb46a279ad32eb6a466606aa753a0e6443
sungslee <k8440009@gmail.com>
2020-10-27 17:47:19 +0900

    add cd home directory move

--- src/cd.c
+++ src/cd.c
@@ -1,4 +1,12 @@
 #include "../includes/minishell.h"
+//	이해 불가
+void	set_pwd_and_res(char *path)
+{
+	set_env(path);
+	set_res(0);
+	ft_free_str(&path);
+	return ;
+}
 
 void	cmd_cd(t_command *command)
 {
@@ -6,12 +14,13 @@ void	cmd_cd(t_command *command)
 	int		argc;
 
 	argc = two_ptr_size(command->arg_list);
-	/*
 	if (argc == 0)
 	{
-
+		home_path = ft_strdup(find_value("HOME"));
+		if (chdir(home_path) == -1)
+			return (error_builtin("cd", "", ""));
+		return (set_pwd_and_res(home_path));
 	}
-	*/
 	if (chdir(command->arg_list[0]) == -1)
 	{
 		ft_putstr_fd(command->arg_list[0], 1);
