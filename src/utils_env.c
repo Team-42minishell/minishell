542ab4035fbf02f6f3cd840e5f89be54a1eb5c92
Younghak Shin <yshin@student.42seoul.kr>
2020-10-28 18:45:04 +0900

    modify func about env

--- src/utils_env.c
+++ src/utils_env.c
@@ -1,19 +1,57 @@
 #include "../includes/minishell.h"
 
+int		get_key_idx(char *key)
+{
+	int		idx;
+
+	idx = 0;
+	while (g_envp[idx])
+	{
+		if (ft_startswith(g_envp[idx], key))
+		{
+			if (*(g_envp[idx] + ft_strlen(key)) == '=')
+				return (idx);
+		}
+		idx++;
+	}
+	return (-1);
+}
+
+int		set_env(char *key, char *val)
+{
+	int		idx;
+	char	*item;
+	char	*temp;
+
+	if (!key || !(*key))
+		return (FALSE);
+	idx = get_key_idx(key);
+	temp = ft_strjoin(key, "=");
+	item = ft_strjoin(temp, val);
+	ft_free_str(&temp);
+	if (idx == -1)
+	{
+		ft_realloc_doublestr(&g_envp, item);
+		ft_free_str(&item);
+	}
+	else
+	{
+		ft_free_str(&g_envp[idx]);
+		g_envp[idx] = item;
+	}
+	return (TRUE);
+}
+
 char	*find_value(char *key)
 {
-	t_env	*current;
-	int		len;
+	int		key_idx;
 
-	current = g_env_list;
-	len = ft_strlen(key);
-	while (current->key != NULL && current->value != NULL)
-	{
-		if (ft_strncmp(key, current->key, len) == 0)
-			return (current->value);
-		current++;
-	}
-	return (NULL);
+	if (!g_envp)
+		return (NULL);
+	key_idx = get_key_idx(key);
+	if (key_idx == -1)
+		return (NULL);
+	return (ft_strchr(g_envp[key_idx], '=') + 1);
 }
 
 /*
